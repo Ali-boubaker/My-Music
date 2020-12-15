fd5437fec2bfbed6e99a49745783bb97c9be780e
Ali-boubaker <alibenboubaker111@gmail.com>
2020-12-15 15:59:43 +0100

    music

---
+++ main.js
@@ -0,0 +1,10 @@
+// $('#ali').on('click',function() {
+// 	// body...
+// 	location.replace("https://www.w3schools.com")
+// })
+
+$("#1").text("if you can't create a beat for your song , you can downlowd an instrument , so shoose one of them")
+
+$('#after').text('After you finish this steps , take your equipment of recording (Microphone , Sound car , Casque ) and start working ..')
+
+$("#after1").text('you will find all the plugins below ,you can use them to do the voice correction, voice effect, and also you can do the sound mastering with them')
